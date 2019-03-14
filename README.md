851d05d815ed100459bdd7afc8134ba951cde1a8
braincy <lca1826@163.com>
2019-03-14 14:13:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+### nginx 启动
+
+```bash
+/Tools/server/nginx-1.14.2/sbin/nginx
+```
+
+### php-fpm 启动
+
+```bash
+/Tools/server/php-7.3.3/sbin/php-fpm
+```
---
+++ server/php-7.3.3/include/php/main/build-defs.h
@@ -0,0 +1,88 @@
+/*                                                                -*- C -*-
+   +----------------------------------------------------------------------+
+   | PHP Version 7                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2018 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Stig Sæther Bakken <ssb@php.net>                             |
+   +----------------------------------------------------------------------+
+*/
+
+#define CONFIGURE_COMMAND " './configure'  '--prefix=/Tools/server/php-7.3.3' '--with-config-file-path=/Tools/server/php-7.3.3/etc' '--enable-fpm' '--with-mcrypt' '--enable-mbstring' '--enable-pdo' '--with-curl' '--disable-debug' '--disable-rpath' '--enable-inline-optimization' '--with-bz2' '--with-zlib' '--enable-sockets' '--enable-sysvsem' '--enable-sysvshm' '--enable-pcntl' '--enable-mbregex' '--with-mhash' '--with-pcre-regex' '--with-mysqli' '--with-gd' '--with-jpeg-dir' '--with-freetype-dir' '--enable-calenda'"
+#define PHP_ADA_INCLUDE		""
+#define PHP_ADA_LFLAGS		""
+#define PHP_ADA_LIBS		""
+#define PHP_APACHE_INCLUDE	""
+#define PHP_APACHE_TARGET	""
+#define PHP_FHTTPD_INCLUDE      ""
+#define PHP_FHTTPD_LIB          ""
+#define PHP_FHTTPD_TARGET       ""
+#define PHP_CFLAGS		"$(CFLAGS_CLEAN) -prefer-non-pic -static"
+#define PHP_DBASE_LIB		""
+#define PHP_BUILD_DEBUG		""
+#define PHP_GDBM_INCLUDE	""
+#define PHP_IBASE_INCLUDE	""
+#define PHP_IBASE_LFLAGS	""
+#define PHP_IBASE_LIBS		""
+#define PHP_IFX_INCLUDE		""
+#define PHP_IFX_LFLAGS		""
+#define PHP_IFX_LIBS		""
+#define PHP_INSTALL_IT		""
+#define PHP_IODBC_INCLUDE	""
+#define PHP_IODBC_LFLAGS	""
+#define PHP_IODBC_LIBS		""
+#define PHP_MSQL_INCLUDE	""
+#define PHP_MSQL_LFLAGS		""
+#define PHP_MSQL_LIBS		""
+#define PHP_MYSQL_INCLUDE	"@MYSQL_INCLUDE@"
+#define PHP_MYSQL_LIBS		"@MYSQL_LIBS@"
+#define PHP_MYSQL_TYPE		"@MYSQL_MODULE_TYPE@"
+#define PHP_ODBC_INCLUDE	""
+#define PHP_ODBC_LFLAGS		""
+#define PHP_ODBC_LIBS		""
+#define PHP_ODBC_TYPE		""
+#define PHP_OCI8_SHARED_LIBADD 	""
+#define PHP_OCI8_DIR			""
+#define PHP_OCI8_ORACLE_VERSION		""
+#define PHP_ORACLE_SHARED_LIBADD 	"@ORACLE_SHARED_LIBADD@"
+#define PHP_ORACLE_DIR				"@ORACLE_DIR@"
+#define PHP_ORACLE_VERSION			"@ORACLE_VERSION@"
+#define PHP_PGSQL_INCLUDE	""
+#define PHP_PGSQL_LFLAGS	""
+#define PHP_PGSQL_LIBS		""
+#define PHP_PROG_SENDMAIL	"/usr/sbin/sendmail"
+#define PHP_SOLID_INCLUDE	""
+#define PHP_SOLID_LIBS		""
+#define PHP_EMPRESS_INCLUDE	""
+#define PHP_EMPRESS_LIBS	""
+#define PHP_SYBASE_INCLUDE	""
+#define PHP_SYBASE_LFLAGS	""
+#define PHP_SYBASE_LIBS		""
+#define PHP_DBM_TYPE		""
+#define PHP_DBM_LIB		""
+#define PHP_LDAP_LFLAGS		""
+#define PHP_LDAP_INCLUDE	""
+#define PHP_LDAP_LIBS		""
+#define PEAR_INSTALLDIR         "/Tools/server/php-7.3.3/lib/php"
+#define PHP_INCLUDE_PATH	".:/Tools/server/php-7.3.3/lib/php"
+#define PHP_EXTENSION_DIR       "/Tools/server/php-7.3.3/lib/php/extensions/no-debug-non-zts-20180731"
+#define PHP_PREFIX              "/Tools/server/php-7.3.3"
+#define PHP_BINDIR              "/Tools/server/php-7.3.3/bin"
+#define PHP_SBINDIR             "/Tools/server/php-7.3.3/sbin"
+#define PHP_MANDIR              "/Tools/server/php-7.3.3/php/man"
+#define PHP_LIBDIR              "/Tools/server/php-7.3.3/lib/php"
+#define PHP_DATADIR             "/Tools/server/php-7.3.3/share/php"
+#define PHP_SYSCONFDIR          "/Tools/server/php-7.3.3/etc"
+#define PHP_LOCALSTATEDIR       "/Tools/server/php-7.3.3/var"
+#define PHP_CONFIG_FILE_PATH    "/Tools/server/php-7.3.3/etc"
+#define PHP_CONFIG_FILE_SCAN_DIR    ""
+#define PHP_SHLIB_SUFFIX        "so"
+#define PHP_SHLIB_EXT_PREFIX    ""
